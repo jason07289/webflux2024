fd0468749346565c01e6acdc4612d511960b3940
jason07289 <jason07999@naver.com>
2024-10-29 18:17:09 +0900

    queue

--- flow/src/main/java/com/fastcampus/flow/FlowApplication.java
+++ flow/src/main/java/com/fastcampus/flow/FlowApplication.java
@@ -1,13 +1,23 @@
 package com.fastcampus.flow;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
 
 @SpringBootApplication
-public class FlowApplication {
+@RequiredArgsConstructor
+public class FlowApplication implements ApplicationListener<ApplicationReadyEvent> {
+	private final ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlowApplication.class, args);
 	}
 
+	@Override
+	public void onApplicationEvent(ApplicationReadyEvent event) {
+//		reactiveRedisTemplate.opsForValue().set("testkey", "testvalue").subscribe();
+	}
 }
